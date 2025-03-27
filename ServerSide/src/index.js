285252da6e2d54b1e13389a6ad3f5e5cdd86d9d0
nikhilsingh1911 <nikhilsingh191130@gmail.com>
2025-03-27 21:53:52 +0530

    second commit

--- ServerSide/src/index.js
+++ ServerSide/src/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-// const express = require("express");
-
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
-
-app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT;
+app.use("/api/auth", authRoutes);
+app.use(express.json());
 app.listen(PORT, () => {
   console.log("Server is Running on port " + PORT);
+  connectDB();
 });
