23e417668f4890020e69c49334d63a39b5d16e67
nikhilsingh1911 <nikhilsingh191130@gmail.com>
2025-03-27 21:53:52 +0530

    second commit

--- ServerSide/src/controllers/auth.controller.js
+++ ServerSide/src/controllers/auth.controller.js
@@ -1,5 +1,47 @@
-export const signup = (req, res) => {
-  res.send("Sign Up route");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 8 characters" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "Email already Exits" });
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user Data" });
+    }
+  } catch (error) {
+    console.log("Error in signup", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 export const login = (req, res) => {
   res.send("Login route");
