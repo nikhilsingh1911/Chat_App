a704051c2beb1d6f81e94e9fd8ed6dd46efa6663
nikhilsingh1911 <nikhilsingh191130@gmail.com>
2025-04-05 13:40:07 +0530

    till sign up done

--- Client_side/src/App.jsx
+++ Client_side/src/App.jsx
@@ -1,16 +1,55 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useEffect } from "react";
+import Navbar from "./Components/navbar";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SignUpPage from "./Pages/SignUpPage";
+import LoginPage from "./Pages/LoginPage";
+import SettingPage from "./Pages/SettingPage";
+import ProfilePage from "./ProfilePage";
+import HomePage from "./Pages/HomePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Toaster } from "react-hot-toast";
+import { Loader } from "lucide-react";
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
-function App() {
-  // const [count, setCount] = useState(0);
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  // console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
 
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        ></Route>
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        ></Route>
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        ></Route>
+        <Route path="/settings" element={<SettingPage />}></Route>
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        ></Route>
+      </Routes>
+      <Toaster />
+    </div>
   );
-}
+};
 
 export default App;
--- ServerSide/src/index.js
+++ ServerSide/src/index.js
@@ -4,12 +4,17 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
-
+import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
