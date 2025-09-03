e3484b42635e59b9459953bb4ce23e13f5f09f47
nikhilsingh1911 <khushboosangwan5@gmail.com>
2025-09-03 22:13:57 +0530

    fied minor bug no. 2

--- Client_side/src/Pages/HomePage.jsx
+++ Client_side/src/Pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useChatStore } from "../store/useChatStore";
 import Sidebar from "../Components/Sidebar";
 import ChatContainer from "../Components/ChatContainer";
-import NoChatSelected from "../Cpmponents/NoChatSelected";
+import NoChatSelected from "../Components/NoChatSelected";
 
 const HomePage = () => {
   const { selectedUser } = useChatStore();
